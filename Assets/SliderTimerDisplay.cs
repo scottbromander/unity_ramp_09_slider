f3a1a9941edec26d38ae8180ddfea714e5f44dec
Scott Bromander <scott@primeacademy.io>
2016-06-22 09:25:51 -0500

    initial commit

---
+++ Assets/SliderTimerDisplay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderTimerDisplay : MonoBehaviour {
+	private CountdownTimer countdownTimer;
+	private Slider sliderUI;
+	private int startSeconds = 30;
+
+	// Use this for initialization
+	void Start () {
+		SetupSlider ();
+		SetupTimer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sliderUI.value = countdownTimer.GetProportionTimeRemaining ();
+		print (countdownTimer.GetProportionTimeRemaining ());
+	}
+
+	private void SetupSlider() {
+		sliderUI = GetComponent<Slider> ();
+		sliderUI.minValue = 0;
+		sliderUI.maxValue = 1;
+		sliderUI.wholeNumbers = false;
+	}
+
+	private void SetupTimer() {
+		countdownTimer = GetComponent<CountdownTimer> ();
+		countdownTimer.ResetTimer (startSeconds);
+	}
+}
