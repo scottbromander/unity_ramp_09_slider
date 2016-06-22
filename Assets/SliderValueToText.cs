8f6d1d42c08b7beb86fb9e752589d028054c00c0
Scott Bromander <scott@primeacademy.io>
2016-06-22 08:22:36 -0500

    initial commit

---
+++ Assets/SliderValueToText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+
+public class SliderValueToText : MonoBehaviour {
+	public Slider sliderUI;
+	private Text textSliderValue;
+
+
+	// Use this for initialization
+	void Start () {
+		textSliderValue = GetComponent<Text> ();
+		ShowSliderValue();
+	}
+
+	public void ShowSliderValue(){
+		string sliderMessage = "Slider Value = " + sliderUI.value;
+		textSliderValue.text = sliderMessage;
+	}
+}
